f26174ae774dfa31ef4ad494566e4302debaa043
connection579 <wlswn4118@naver.com>
2020-11-07 18:00:11 +0900

    비트 이동 연산자

---
+++ src/operationTest6/OperationTest6.java
@@ -0,0 +1,16 @@
+package operationTest6;
+
+public class OperationTest6 {
+
+	public static void main(String[] args) {
+		int num = 0B0000101;
+		System.out.println(num <<2);
+		System.out.println(num >>2);
+		System.out.println(num >>>2);
+		System.out.println(num);
+		num = num<<2;
+		System.out.println(num);
+
+	}
+
+}
